696c89a846527c8057a86dc7e78e5282eafbfce6
Dennis <38273998+Dennishaha@users.noreply.github.com>
2026-06-29 00:46:21 +0800

    feat: add pine worker process launcher

---
+++ pkg/strategy/pineworker/grpc_dialer.go
@@ -0,0 +1,59 @@
+package pineworker
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type GRPCDialerConfig struct {
+	MaxMessageBytes int
+	DialOptions     []grpc.DialOption
+}
+
+type GRPCDialer struct {
+	config GRPCDialerConfig
+}
+
+func NewGRPCDialer(config GRPCDialerConfig) *GRPCDialer {
+	if config.MaxMessageBytes <= 0 {
+		config.MaxMessageBytes = DefaultWorkerConfig(1).MaxMessageBytes
+	}
+	return &GRPCDialer{config: config}
+}
+
+func (dialer *GRPCDialer) Dial(ctx context.Context, address string) (ManagedTransport, error) {
+	if dialer == nil {
+		return nil, fmt.Errorf("pine worker grpc dialer is nil")
+	}
+	options := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultCallOptions(
+			grpc.MaxCallRecvMsgSize(dialer.config.MaxMessageBytes),
+			grpc.MaxCallSendMsgSize(dialer.config.MaxMessageBytes),
+		),
+	}
+	options = append(options, dialer.config.DialOptions...)
+	conn, err := grpc.NewClient(address, options...)
+	if err != nil {
+		return nil, err
+	}
+	return &managedGRPCTransport{
+		GRPCTransport: NewGRPCTransport(conn),
+		conn:          conn,
+	}, nil
+}
+
+type managedGRPCTransport struct {
+	*GRPCTransport
+	conn *grpc.ClientConn
+}
+
+func (transport *managedGRPCTransport) Close() error {
+	if transport == nil || transport.conn == nil {
+		return nil
+	}
+	return transport.conn.Close()
+}
